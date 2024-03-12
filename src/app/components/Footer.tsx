ff7348f7df911a7e5a4d9bc32de97964e7550afe
Shinichi Haramizu <shinichi@haramizu.jp>
2024-03-12 09:15:22 +0900

    update header and footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,30 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <>
-      <footer>Footer Content</footer>
+      <footer>
+        <div className="mx-auto max-w-7xl px-6 pb-8 ">
+          <div
+            className="mt-8 border-t border-gray
+           dark:border-gray-800 pt-8 md:flex md:items-center md:justify-between"
+          >
+            <div className="flex space-x-6 md:order-2">
+              <a
+                key="GitHub"
+                href="https://github.com/haramizu/Nextjs-Components"
+                className="text-gray-500 hover:text-gray-400"
+              >
+                <span className="sr-only">GitHub</span>
+                <FaGithub />
+              </a>
+            </div>
+            <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0 ">
+              &copy; 2024 Shinichi Haramizu, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,48 @@
+import {
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export default function Header() {
   return (
     <>
       <header>
-        <ThemeSwitcher />
+        <Navbar>
+          <NavbarBrand>
+            <p className="font-bold text-inherit">
+              <Link href="/">Next.js Components</Link>
+            </p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <Link href="/Breadcrumb" color="foreground">
+                BreadCrumb
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link href="Carousel" color="foreground">
+                Carousel
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="/CodeBlock">
+                CodeBlock
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem className="hidden lg:flex">
+              <Link href="#">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <ThemeSwitcher />
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
       </header>
     </>
   );
